76ed7f27835807f3370228fef61bea6e1cb47b95
nicholas jackson <nicdogg360@gmail.com>
2021-01-29 17:09:05 -0800

    added a dark mode

--- main/app.js
+++ main/app.js
@@ -1,20 +1,15 @@
 let myLibrary = [];  
 
-let bookList = [];
 
 const title_form = document.querySelector(".form_input");  
 const author_form = document.querySelector(".author_form");
 const publish_form = document.querySelector(".publish_form"); 
-
-let delete_btns;  
-
-
-
-
 const submit_btn = document.querySelector(".submit_btn");  
+const dark_btn = document.querySelector(".dark_btn"); 
+const doc_body = document.querySelector("body"); 
+
 
 const bookDisplay = document.querySelector(".grid");
-
 
 function Book(title,author,publishingDate){ 
     this.title = title; 
@@ -27,18 +22,15 @@ Book.prototype.addBookToLibraryArray = function(){
     myLibrary.push(this);
 } 
 
-Book.prototype.addBookToDisplay = function(){ 
 
+Book.prototype.addBookToDisplay = function(){ 
     var bookContainer = document.createElement("div");  
 
-  
     var titleContainer = document.createElement("p"); 
     var authorContainer = document.createElement("p"); 
     var readContainer = document.createElement("button");  
     var deleteContainer = document.createElement("button");    
 
-
-    bookContainer.appendChild(titleContainer);
     bookContainer.appendChild(titleContainer);
     bookContainer.appendChild(authorContainer);
     bookContainer.appendChild(readContainer);
@@ -56,11 +48,7 @@ Book.prototype.addBookToDisplay = function(){
     deleteContainer.classList.add("delete_btn");
     bookDisplay.appendChild(bookContainer); 
 
-    bookList.push(bookContainer);
-    delete_btns = document.querySelectorAll(".delete_btn"); 
-
-} 
-
+}  
 
 
  function hasBook(title, author){
@@ -70,12 +58,6 @@ Book.prototype.addBookToDisplay = function(){
     }) 
     return hasBook;
 }
-
-function displayBooksArray(){
-    myLibrary.forEach((book, index) =>{
-        console.log(book);
-    })
-}  
 
 function checkInput(title, author, publish){ 
     if((title == "" || author == "" || publish == "")){ 
@@ -89,16 +71,50 @@ function deleteBook(btn){
 
     myLibrary.forEach((book, index) =>{
         if(book.title == bookTitle){
-            myLibrary.splice(index,1);  
-            bookList.splice(index,1);  
+            myLibrary.splice(index,1); 
             btn.parentNode.remove();
-            console.log(myLibrary); 
-            console.log(bookList); 
-            console.log(bookDisplay);
         }
     })
 }
  
+function resetForm(){
+    let title = title_form;   
+    let author = author_form; 
+    let publish = publish_form; 
+    
+    title.value = ''; 
+    author.value = ''; 
+    publish.value = '';
+
+}
+
+
+
+submit_btn.addEventListener('click',() =>{
+    let title = title_form.value;   
+    let author = author_form.value; 
+    let publish = publish_form.value;     
+    if(checkInput(title,author,publish)) return;
+    if((hasBook(title,author))) return ;
+    let book = new Book(title,author,publish); 
+    book.addBookToLibraryArray(); 
+    book.addBookToDisplay();  
+    resetForm();
+})   
+dark_btn.addEventListener('click', () =>{
+    doc_body.classList.toggle("dark-mode");
+})
+
+bookDisplay.addEventListener('click', (e) =>{
+    e.stopPropagation();
+    if(e.target && e.target.classList == "btn delete_btn"){
+       deleteBook(e.target);
+    } 
+    else if(e.target && e.target.classList == "btn read_btn"){
+       console.log("pressed the read btn");
+    } 
+})
+
 
 let book1 = new Book("Naruto", "Musashi Kishimoto", "1999"); 
 let book2 = new Book("Hunter x Hunter", "Yoshihiro Togashi", "1999"); 
@@ -107,37 +123,6 @@ book1.addBookToLibraryArray();
 book1.addBookToDisplay();
 book2.addBookToLibraryArray(); 
 book2.addBookToDisplay();
-
-
-submit_btn.addEventListener('click',() =>{
-    let title = title_form.value;   
-    let author = author_form.value; 
-    let publish = publish_form.value;     
-
-    if(checkInput(title,author,publish)) return;
-    if((hasBook(title,author))) return ;
-
-    let book = new Book(title,author,publish); 
-    book.addBookToLibraryArray(); 
-    displayBooksArray(); 
-    book.addBookToDisplay(); 
-})   
-
-// delete_btns.forEach(btn =>{
-//     btn.addEventListener('click',() =>{
-//         deleteBook(btn);
-//     })
-// }) 
-
-
-
-
-bookDisplay.addEventListener('click', (e) =>{
-    e.stopPropagation();
-    if(e.target && e.target.classList == "btn delete_btn"){
-       deleteBook(e.target);
-    }
-})
 
 
 
